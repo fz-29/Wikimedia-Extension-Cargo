203072e5e865d7a594b5e7eababb9eff9b3f1f08
Feroz <f29ahmad@gmail.com>
2017-06-21 02:40:10 +0530

    Space issue CH solved
    Change-Id: Ic955ddd45b597098573fa0ea27acdc939009223b

--- CargoHierarchy.php
+++ CargoHierarchy.php
@@ -1,17 +1,18 @@
 <?php
-/**
- * CargoHierarchy - holds the tree for hierarchy inorder to implement Nested Set model for
- * efficient storage and query of hierarchy fields.
- *
- * @author Feroz Ahmad
- * @ingroup Cargo
- */
+	/**
+	* CargoHierarchy - holds the tree for hierarchy inorder to implement Nested Set model for
+	* efficient storage and query of hierarchy fields.
+	*
+	* @author Feroz Ahmad
+	* @ingroup Cargo
+	*/
 
 class CargoHierarchy {
-    public $mTitle;
-    public $mChildren;
-    public $mLeft = 0;
-    public $mRight = 0;
+	
+	public $mTitle;
+	public $mChildren;
+	public $mLeft = 0;
+	public $mRight = 0;
 
 	function __construct( $curTitle = '__pseudo_root__' ) {
 		$this->mTitle = $curTitle;
@@ -23,14 +24,14 @@ class CargoHierarchy {
 	}
 
 	/**
-	 * Turn a manually-created "structure", defined as a bulleted list
-	 * in wikitext, into a tree. This code has been borrowed from PFTree class
-     * of PageForms Extension
-	 */
+	* Turn a manually-created "structure", defined as a bulleted list
+	* in wikitext, into a tree. This code has been borrowed from PFTree class
+	* of Page Forms Extension
+	*/
 	public static function newFromWikiText( $wikitext ) {
-        // A dummy node (__pseudo_root__ is added so that
-        // multiple nodes can be added in the first level
-        $fullTree = new CargoHierarchy();
+		// A dummy node (__pseudo_root__ is added so that
+		// multiple nodes can be added in the first level
+		$fullTree = new CargoHierarchy();
 		$lines = explode( "\n", $wikitext );
 		foreach ( $lines as $line ) {
 			$numBullets = 0;
@@ -47,40 +48,40 @@ class CargoHierarchy {
 
 	function getLastNodeForLevel( $level ) {
 		if ( $level <= 1 || count( $this->mChildren ) == 0 ) {
-			return $this;
+		return $this;
 		}
 		$lastNodeOnCurLevel = end( $this->mChildren );
 		return $lastNodeOnCurLevel->getLastNodeForLevel( $level - 1 );
 	}
 
-    function generateHierarchyStructureTableData() {
-        $tableData = array();
-        $this->computeLeftRight();
-        //  Preorder traversal using Stack data structure
-        $stack = new SplStack();
-        $stack->push( $this );
-        while( !$stack->isEmpty() ) {
-            $node = $stack->pop();
-            $row = array();
-            $row['_value'] = $node->mTitle;
-            $row['_left'] = $node->mLeft;
-            $row['_right'] = $node->mRight;
-            $tableData[] = $row;
-            foreach( array_reverse( $node->mChildren ) as $child ) {
-                $stack->push( $child );
-            }
-        }
-        return $tableData;
-    }
+	function generateHierarchyStructureTableData() {
+		$tableData = array();
+		$this->computeLeftRight();
+		//  Preorder traversal using Stack data structure
+		$stack = new SplStack();
+		$stack->push( $this );
+		while( !$stack->isEmpty() ) {
+			$node = $stack->pop();
+			$row = array();
+			$row['_value'] = $node->mTitle;
+			$row['_left'] = $node->mLeft;
+			$row['_right'] = $node->mRight;
+			$tableData[] = $row;
+			foreach( array_reverse( $node->mChildren ) as $child ) {
+				$stack->push( $child );
+			}
+		}
+		return $tableData;
+	}
 
-    private function computeLeftRight( &$counter = 1 ) {
-        $this->mLeft = $counter;
-        $counter += 1;
-        //  Visit mChildren of the current node
-        foreach( $this->mChildren as $child ) {
-            $child->computeLeftRight( $counter );
-        }
-        $this->mRight = $counter;
-        $counter += 1;
-    }
+	private function computeLeftRight( &$counter = 1 ) {
+		$this->mLeft = $counter;
+		$counter += 1;
+		//  Visit mChildren of the current node
+		foreach( $this->mChildren as $child ) {
+			$child->computeLeftRight( $counter );
+		}
+		$this->mRight = $counter;
+		$counter += 1;
+	}
 }
